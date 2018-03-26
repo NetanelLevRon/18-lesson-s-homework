59772c5d620948e31851661fc5166b59b9323e04
Netanel Lev-Ron <NetanelLevRonfg@gmail.com>
2018-03-26 03:51:55 +0300

    multiplication table

--- angular/34 lesson homework/exercise 1/src/app/app.component.ts
+++ angular/34 lesson homework/exercise 1/src/app/app.component.ts
@@ -5,6 +5,25 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+
+public mat:Array<Array<number>>=new Array<Array<number>>();
+
+
+func():void{
+
+  for(let i=0;i<10;i++){
+    this.mat.push([i]);
+    for(let j=0;j<10;j++){   
+  this.mat[i][j]=((j+1)*(i+1));
+    }
+  }
+
+}
+
+constructor(){
+  this.func();
+}
+
 }
