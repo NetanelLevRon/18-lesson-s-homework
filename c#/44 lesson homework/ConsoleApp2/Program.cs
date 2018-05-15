cc7e70c56cbc66d89a49b111fc264cfdeb69ef07
Netanel Lev-Ron <NetanelLevRonfg@gmail.com>
2018-05-15 16:22:49 +0300

    gap between two numbers

---
+++ c#/44 lesson homework/ConsoleApp2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int temp;
+            Console.Write("please enter first number: ");
+            temp=int.Parse(Console.ReadLine());
+
+            if (temp > -99 && temp < 99)
+            {
+                double firstNum = temp;
+                Console.Write("please enter second number: ");
+                temp = int.Parse(Console.ReadLine());
+                if (temp > -99 && temp < 99)
+                {
+                    double secondNum = temp;
+                    Console.WriteLine($"the gap between '{firstNum}' to '{secondNum}' is: {Math.Abs(firstNum-secondNum)}.");
+                }
+                else
+                {
+                    Console.WriteLine("please enter two char max!!");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("please enter two char max!!");
+            }
+        }
+    }
+}
